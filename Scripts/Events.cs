678818bc33269dc65c34b17dd1c3d6a3a1d8241b
VihangiSalgado <63911935+VihangiSalgado@users.noreply.github.com>
2020-05-10 20:59:16 +0530

    Update 1

---
+++ Scripts/Events.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Events : MonoBehaviour
+{
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+}
