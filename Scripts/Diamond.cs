297834cb3b34156f0ee72bca24a85c64df3db99b
VihangiSalgado <63911935+VihangiSalgado@users.noreply.github.com>
2020-05-12 11:21:32 +0530

    Update3

---
+++ Scripts/Diamond.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamond : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 40 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerManager.numberOfDiamond += 1;
+            Debug.Log("numberOfDiamon:" + PlayerManager.numberOfDiamond);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Scripts/MainMenue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenue : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
